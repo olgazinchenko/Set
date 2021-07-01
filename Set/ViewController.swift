8b085fc7f721dc55d41d55da4d4bdb26e191a2a0
Olga Zinchenko <ozinchenko.pm@gmail.com>
2021-07-01 21:35:02 +0300

    added addCardsStackViews outlet collection and increased add card functionality

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController {
         cards = []
         cards = cardsGenerator()
         renderCards()
-        inGameCards = pickInGameCards(amount: 15)
+        inGameCards = pickInGameCards(amount: cardButtons.count)
         updateViewFromModel()
+        hideAdditionalCards()
     }
     @IBOutlet var cardButtons: [UIButton]!
     @IBAction func touchCard(_ sender: UIButton) {
@@ -25,6 +26,8 @@ class ViewController: UIViewController {
         }
     }
     @IBOutlet weak var addCardsStackView: UIStackView!
+    @IBOutlet var addCardsStackViews: [UIStackView]!
+    
     @IBAction func addCardsButton(_ sender: UIButton) {
         addCards()
     }
@@ -68,11 +71,16 @@ class ViewController: UIViewController {
     }
     
     func renderCards() {
-        addCardsStackView.isHidden = true
         for index in cardButtons.indices {
             cardButtons[index].layer.borderWidth = 3.0
             cardButtons[index].layer.borderColor = UIColor.gray.cgColor
             cardButtons[index].layer.cornerRadius = 8.0
+        }
+    }
+    
+    func hideAdditionalCards() {
+        for index in addCardsStackViews.indices {
+            addCardsStackViews[index].isHidden = true
         }
     }
     
@@ -111,7 +119,12 @@ class ViewController: UIViewController {
     
     func addCards() {
         UIView.animate(withDuration: 0.5, animations:  {
-            self.addCardsStackView.isHidden = false
+            for index in self.addCardsStackViews.indices {
+                if self.addCardsStackViews[index].isHidden == true {
+                    self.addCardsStackViews[index].isHidden = false
+                    break
+                }
+            }
         })
     }
     
@@ -120,9 +133,8 @@ class ViewController: UIViewController {
     
     //You will also need a “Deal 3 More Cards” button (as per the rules of Set).
     //When the Deal 3 More Cards button is pressed either a) replace the selected cards if they are a match or b) add 3 cards to the game.
-    //The Deal 3 More Cards button should be disabled if there are a) no more cards in the Set deck or b) no more room in the UI to fit 3 more cards (note that there is always room for 3 more cards if the 3 currently-selected cards are a match since you replace them).
     
-    //Deal 12 cards only to start. They can appear anywhere on screen (i.e. they don’t have to be aligned at the top or bottom or anything; they can be scattered to start if you want), but should not overlap.
+    //The Deal 3 More Cards button should be disabled if there are a) no more cards in the Set deck or b) no more room in the UI to fit 3 more cards (note that there is always room for 3 more cards if the 3 currently-selected cards are a match since you replace them).
     
     //Like you did for Concentration, you must have a New Game button and show the Score in the UI. It is up to you how you want to score your Set game. For example, you could give 3 points for a match and -5 for a mismatch and maybe even -1 for a deselection. Perhaps fewer points are scored depending on how many cards are on the table (i.e. how many times Deal 3 More Cards has been touched). Whatever you think best evaluates how well the player is playing.
 
